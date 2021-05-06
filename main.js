cf4a84ea713b52ba5cec208a8e8644d727580e82
Nouha Menadjlia <77000425+nouhamenadjlia@users.noreply.github.com>
2021-05-06 14:03:21 +0200

    Add files via upload
    adding project section

--- main.js
+++ main.js
@@ -1,29 +1,64 @@
-/*const shutMenu= document.querySelector(".shut-menu");
-let menuOpen = open;
-shutMenu.addEventListener('click', () => {
-    shutMenu.classList.remove('open');
-    menuOpen = false;
-    
-});*/
-/*let open = false;
-$('.shut-menu').on('click', function(){
-   if (open == false){
-    $('.menu').show()
-      open = true;
-   }else{
-    $('.menu').hide(); 
-      open = false;
-   }
-});*/
+/*------------------------------------fermeture du menu -----------------------------------------------*/
 $('.shut-menu').on('click', function(){
   $('.toggler').click();
   });
-  
- 
+  /*-------------------------------projets-----------------------------------------------*/
+
+  $(document).ready(function(){
+
+  const items = document.querySelectorAll('img');
+  const nbSlide = items.length;
+  const suivant = document.querySelector('.right');
+  const precedent = document.querySelector('.left');
+    let count = 0;
+
+  function slideSuivante(){
+    items[count].classList.remove('active');
+
+    if(count < nbSlide - 1){
+        count++;
+    } else {
+        count = 0;
+    }
+
+    items[count].classList.add('active')
+    console.log(count);
+    
+  }
+suivant.addEventListener('click', slideSuivante)
+
+
+ function slidePrecedente(){
+  items[count].classList.remove('active');
+
+  if(count > 0){
+    count--;
+  } else {
+    count = nbSlide - 1;
+  }
+
+  items[count].classList.add('active')
+  // console.log(count);
+    
+}
+precedent.addEventListener('click', slidePrecedente)
+
+function keyPress(e){
+  console.log(e);
+    
+  if(e.keyCode === 37){
+    slidePrecedente();
+} else if(e.keyCode === 39){
+  slideSuivante();
+  }
+}
+ document.addEventListener('keydown', keyPress)
+  })
 
 
 
 
+/*---------------------------------------------skils-----------------------------------------------*/
 const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper');
 
 portfolioItems.forEach(portfolioItem => {
